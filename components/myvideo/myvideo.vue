d3b50a4ca8c26737cf106db72f154969317d7fc0
daijiaying1 <1773292109@qq.com>
2025-08-13 18:29:49 +0800

    页面

---
+++ components/myvideo/myvideo.vue
@@ -0,0 +1,86 @@
+<template>
+	<view>
+		<view class="tabs">
+			<view v-for="(item,index) in tabs" :class="current == index ? 'current':''" @click="current = index">
+				{{item}}</view>
+		</view>
+		<view class="con">
+			<view class="list"  v-for="(item,index) in 9" :key="index">
+				<image src="/static/tsp-icon/touxiang.jpg" mode="widthFix"></image>
+				<view class="play">
+					<text>播放：11.2w</text>
+					<text>01:01:01</text>
+				</view>
+				<view class="title">"男生组队打游戏打游戏"</view>
+				<view class="time">2025-02-02 18:00:00</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script setup>
+	import {
+		ref,
+		reactive
+	} from 'vue'
+	const current = ref(0)
+	const tabs = reactive(['短视频', '长视频'])
+</script>
+
+<style lang="scss" scoped>
+	.tabs {
+		display: flex;
+
+		view {
+			margin-right: 15rpx;
+			color: rgb(255, 255, 255, .8);
+		}
+
+		.current {
+			color: #ffffff;
+			font-size: 34rpx;
+		}
+	}
+   .con{
+	 column-count: 2; //一行几个
+	 column-gap: 30rpx;  //行间距
+	 margin-top: 30rpx;
+   }
+	.list {
+		break-inside: avoid;//避免在主体框中插入任何中断（页面，列或区域）。
+		background: #212028;
+		margin-bottom: 20rpx;
+		width: 100%;
+		border-radius: 15rpx;
+		overflow: hidden;
+
+		image {
+			width: 100%;
+		}
+
+		.play {
+			position: relative;
+			font-size: 20rpx;
+			margin: -50rpx 0 20rpx 0;
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.time {
+			color: rgb(255, 255, 255, .8);
+			font-size: 20rpx;
+			margin: 15rpx 25rpx;
+		}
+
+		.title {
+			overflow: hidden;
+			/* 超出隐藏   */
+			white-space: nowrap;
+			/* 不换行     */
+			text-overflow: ellipsis;
+			/* 省略号     */
+			margin: 35rpx 25rpx 0 15rpx;
+			font-size: 27rpx;
+		}
+
+	}
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,13 +1,91 @@
 <template>
-	<view>
-		my
-	</view>
+	  <z-paging ref="paging" v-model="dataList" @query="queryList">
+		<up-navbar bgColor="transparent" placeholder :autoBack="false" :fixed="false">
+			<template #right>
+				<text class="">消息中心</text>
+				<text class="setup">设置</text>
+			</template>
+		</up-navbar>
+		<view class="content">
+			<userinfo />
+			<vip />
+			<!-- 动态，视频，互动 -->
+			<view class="tabs">
+				<view v-for="(item,index) in tarbar" :key="index" :class="current == index ? 'current':''"
+					@click="clicks(index)">{{item}}</view>
+			</view>
+			<active v-if="current == 0"/>
+			<myvideo v-if="current == 1"/>
+			<interact v-if="current == 2"/>
+		</view>
+	</z-paging>
 </template>
 
 <script setup>
+	import {
+		ref , reactive
+	} from 'vue';
+	import userinfo from './components/userinfo.vue'
+	import vip from './components/vip.vue'
+	import tabs from '@/components/tabs/tabs.vue'
+	const list = ref([{
+		name: '消息中心'
+	}, {
+		name: '设置'
+	}])
+	const tarbar = reactive(['动态','视频','互动'])
+	const current = ref(0)
+	const clicks = (index) => {
+		current.value = index
+	}
+	const paging = ref(null)
+		// v-model绑定的这个变量不要在分页请求结束中自己赋值，直接使用即可
+	  const dataList = ref([])
+	  
+		// @query所绑定的方法不要自己调用！！需要刷新列表数据时，只需要调用paging.value.reload()即可
+	  const queryList = (pageNo, pageSize) => {
+			// 此处请求仅为演示，请替换为自己项目中的请求
+	//       request.queryList({ pageNo,pageSize }).then(res => {
+			// 	// 将请求结果通过complete传给z-paging处理，同时也代表请求结束，这一行必须调用
+	//           paging.value.complete(res.data.list);
+	//       }).catch(res => {
+			// 	// 如果请求失败写paging.value.complete(false);
+			// 	// 注意，每次都需要在catch中写这句话很麻烦，z-paging提供了方案可以全局统一处理
+			// 	// 在底层的网络请求抛出异常时，写uni.$emit('z-paging-error-emit');即可
+				paging.value.complete([{title:'123'},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"},{title:"2243"}]);
+			// })
+	  }
+	  const onTabChange = (tab) => {
+	  	console.log(tab);
+	  }
 	
 </script>
 
-<style>
-	       
+<style lang="scss" scoped>
+	.content {
+		// background: pink;
+		margin: 30rpx;
+	}
+	.setup{
+		margin-left: 30rpx;
+	}
+	.tabs{
+		display: flex;
+		margin: 30rpx 0;
+		
+		view{
+			width: 33%;
+			text-align: center;
+			
+			height: 70rpx;
+			line-height: 70rpx;
+			border-radius: 80rpx;
+			color: #fff;
+			
+		}
+		.current{
+			background: #ffffff;
+			color: #000;
+		}
+	} 
 </style>
