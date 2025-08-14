6ee60e678ea46f5ca2d52c452916fb80fa9ce048
daijiaying1 <1773292109@qq.com>
2025-08-14 15:06:11 +0800

    画页面

--- pages/my/data-editing.vue
+++ pages/my/data-editing.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="">
-		<up-navbar bgColor="transparent" placeholder :autoBack="false" :fixed="false">
+		<up-navbar bgColor="transparent" placeholder :autoBack="true" :fixed="false">
 			<template #left>
 				<up-icon name="arrow-left" color="#ffffff" size="20"></up-icon>
 			</template>
