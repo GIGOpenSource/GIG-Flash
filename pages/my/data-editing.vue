03730eff5fb8477b5722c0fdb880fc9305782db8
daijiaying1 <1773292109@qq.com>
2025-08-14 12:51:49 +0800

    提交

---
+++ pages/my/data-editing.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="">
+		<up-navbar bgColor="transparent" placeholder :autoBack="false" :fixed="false">
+			<template #left>
+				<up-icon name="arrow-left" color="#ffffff" size="20"></up-icon>
+			</template>
+			<template #center>
+				资料编辑
+			</template>
+			<template #right>
+				保存
+			</template>
+		</up-navbar>
+		       <view class="avatar">
+				    <up-image :show-loading="true" :src="src" width="80px" height="80px" shape="circle" ></up-image>
+			   </view>
+		<view class="form" v-for="(item,index) in form" :key="index">
+			<text>{{item.name}}</text>
+			<up-textarea  v-if="index == 2" v-model="item.key" placeholder="请输入内容" count maxlength="20" border="none"></up-textarea>
+			<input type="text"  v-else  :placeholder="'请输入' + item.name" v-model="item.key"/>
+			
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref,
+		reactive
+	} from 'vue'
+	const src = ref('https://cdn.uviewui.com/uview/album/1.jpg');
+	const form = reactive([{
+		name: '昵称',
+		type: 'input',
+		key: 'nickname'
+	}, {
+		name: 'ID',
+		type: 'input',
+		key: 'id'
+	}, {
+		name: '签名',
+		type: 'textera',
+		key: 'sign'
+	}])
+	const click = () => {
+	  // 点击事件处理逻辑
+	};
+</script>
+
+<style lang="scss" scoped>
+	.avatar{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 50rpx 0 30rpx 0;
+	}
+	.form{
+		display: flex;
+		margin: 20rpx;
+		input{
+			background: #212028;
+			margin-left: 20rpx;
+			padding: 15rpx 20rpx;
+			width: 80%;
+			border-radius: 10rpx;
+		}
+		text{
+			display: block;
+			width: 80rpx;
+		}
+	}
+	.u-textarea{
+		margin-left: 20rpx;
+		background-color: #212028;
+	}
+</style>
+
+<style>
+	 .u-textarea__count{
+		 background-color: #212028 !important;
+	 }
+</style>
---
+++ pages/my/setup.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="">
+		<up-navbar bgColor="transparent" placeholder :autoBack="false" :fixed="false">
+			<template #left>
+				<up-icon name="arrow-left" color="#ffffff" size="20"></up-icon>  
+			</template>
+			<template #center>
+				设置
+			</template>
+			<template #right>
+				<up-icon name="more-dot-fill" color="#ffffff" size="28"></up-icon>
+			</template>
+		</up-navbar>
+		<view class="form" v-for="(item,index) in form" :key="index" @click="toPath(item.path)">
+			<view class="">{{item.title}}</view>
+			<up-icon name="arrow-right" color="#ffffff" size="20"></up-icon>  
+		</view>
+	</view>
+</template> 
+
+<script setup>
+import {ref,reactive} from 'vue'
+const form = reactive([{
+	title:'资料编辑',
+	path:'/pages/my/data-editing'
+},{
+	title:'任务编辑',
+	path:''
+},{
+	title:'消费与充值',
+	path:''
+},{
+	title:'我的钱包',
+	path:''
+},{
+	title:'在线客服',
+	path:''
+},{
+	title:'邀请码填写',
+	path:''
+},{
+	title:'创作者申请（后期上线）',
+	path:''
+}])
+ const toPath = (url) => {
+		   uni.navigateTo({
+		   	url
+		   })
+	  }
+</script>
+
+<style lang="scss" scoped>
+	.form{
+		display: flex;
+		padding: 30rpx 0;
+		justify-content: space-between;
+		margin: 0 30rpx;
+		border-bottom: 1rpx solid rgba(255, 255, 255, .1);
+	}
+	.form:nth-of-type(8){
+		border-bottom: none;
+	}
+</style>
