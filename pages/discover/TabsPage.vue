404654503516f8e98d1b96d7d5752dc94983bdce
wyz <wyz@gig.com>
2025-08-20 16:12:59 +0800

    feat 修改发现页布局

---
+++ pages/discover/TabsPage.vue
@@ -0,0 +1,55 @@
+<template>
+	<z-paging ref="pagingRef" v-model="dataList" @query="queryList">
+		<view class="content">
+
+			<BannerSwiper />
+
+			<view class="card-list">
+				<card-view v-for="item in 10" @click="handleToLongVideo"></card-view>
+			</view>
+
+		</view>
+	</z-paging>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from 'vue';
+
+	defineProps(['tabIndex', 'current'])
+
+	import BannerSwiper from './components/BannerSwiper/BannerSwiper.vue';
+	import CardView from './components/CardView/CardView.vue';
+
+	const dataList = ref([])
+
+	const queryList = () => {}
+
+	const handleToLongVideo = () => {
+		uni.navigateTo({
+			url: '/pages/video/video'
+		})
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		padding: 20rpx;
+	}
+
+	.card-list {
+		display: flex;
+		flex-wrap: wrap;
+		margin-top: 20rpx;
+
+		.card-view {
+			margin-right: 30rpx;
+			margin-bottom: 30rpx;
+
+			&:nth-child(2n) {
+				margin-right: 0;
+			}
+		}
+	}
+</style>
--- pages/discover/discover.vue
+++ pages/discover/discover.vue
@@ -1,5 +1,5 @@
 <template>
-	<z-paging ref="paging" :loading-more-enabled="false" v-model="dataList" @query="queryList">
+	<z-paging-swiper ref="paging" :loading-more-enabled="false">
 		<template #top>
 
 			<!-- 导航栏 -->
@@ -30,27 +30,15 @@
 
 		</template>
 
+		<swiper :current="currentCategory" style="height: 100%" @animationfinish="swiperAnimationfinish">
+			<swiper-item v-for="(item, index) in categoryList.length">
+				<tabs-page :current="currentCategory" :tabsIndex="index"></tabs-page>
+			</swiper-item>
 
-		<!-- z-paging默认铺满全屏，此时页面所有view都应放在z-paging标签内，否则会被盖住 -->
-		<!-- 需要固定在页面顶部的view请通过slot="top"插入，包括自定义的导航栏 -->
-		<view class="content">
-			<swiper :current="currentCategory" style="height: calc(100vh - 90px)">
-				<swiper-item v-for="item in categoryList.length">
-					<z-paging>
-						<BannerSwiper />
-						<view class="card-list">
-							<card-view v-for="item in 10" @click="handleToLongVideo"></card-view>
-						</view>
-					</z-paging>
+		</swiper>
 
 
-				</swiper-item>
-
-			</swiper>
-
-		</view>
-
-	</z-paging>
+	</z-paging-swiper>
 </template>
 
 <script setup>
@@ -59,8 +47,7 @@
 		onMounted
 	} from 'vue'
 	import tabs from '@/components/tabs/tabs.vue'
-	import CardView from './components/CardView/CardView.vue'
-	import BannerSwiper from './components/BannerSwiper/BannerSwiper.vue'
+	import TabsPage from './TabsPage.vue'
 
 	import {
 		getAdsList
@@ -72,11 +59,11 @@
 			currentPage: 1,
 			pageSize: 20
 		}
-		getAdsList(params).then(res => {
-			console.log('res', res);
-		}).catch(err => {
-			console.log('err', err);
-		})
+		// getAdsList(params).then(res => {
+		// 	console.log('res', res);
+		// }).catch(err => {
+		// 	console.log('err', err);
+		// })
 	})
 
 	const list = ref([{
@@ -128,9 +115,7 @@
 	])
 	// 切换分类
 	const handleClickCategory = (tab) => {
-		console.log(tab);
 		currentCategory.value = tab.index
-		console.log('currentCategory', currentCategory.value);
 	}
 	const onTabChange = (tab) => {
 		console.log(tab);
@@ -149,72 +134,12 @@
 		console.log('leftClick');
 	};
 
-	const paging = ref(null)
-	// v-model绑定的这个变量不要在分页请求结束中自己赋值，直接使用即可
-	const dataList = ref([])
-
-	// @query所绑定的方法不要自己调用！！需要刷新列表数据时，只需要调用paging.value.reload()即可
-	const queryList = (pageNo, pageSize) => {
-		// 此处请求仅为演示，请替换为自己项目中的请求
-		//       request.queryList({ pageNo,pageSize }).then(res => {
-		// 	// 将请求结果通过complete传给z-paging处理，同时也代表请求结束，这一行必须调用
-		//           paging.value.complete(res.data.list);
-		//       }).catch(res => {
-		// 	// 如果请求失败写paging.value.complete(false);
-		// 	// 注意，每次都需要在catch中写这句话很麻烦，z-paging提供了方案可以全局统一处理
-		// 	// 在底层的网络请求抛出异常时，写uni.$emit('z-paging-error-emit');即可
-		paging.value.complete([{
-			title: '123'
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}, {
-			title: "2243"
-		}]);
-		// })
-	}
-
-	const handleToLongVideo = () => {
-		uni.navigateTo({
-			url: '/pages/video/video'
-		})
+	// swiper滑动结束
+	const swiperAnimationfinish = (e) => {
+		currentCategory.value = e.detail.current;
 	}
 </script>
 
 <style lang="scss">
-	.content {
-		padding: 20rpx;
-	}
 
-	.card-list {
-		display: flex;
-		flex-wrap: wrap;
-		margin-top: 20rpx;
-
-		.card-view {
-			margin-right: 30rpx;
-			margin-bottom: 30rpx;
-
-			&:nth-child(2n) {
-				margin-right: 0;
-			}
-		}
-	}
 </style>
