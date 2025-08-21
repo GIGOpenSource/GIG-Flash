ae183c4cf3bafd10460090fdfbe19ca4a090ab53
wyz <wyz@gig.com>
2025-08-21 14:30:17 +0800

    欢迎页接口对接 & 修改 分享功能为复制

--- pages/welecome/index.vue
+++ pages/welecome/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="page">
-		<scroll-view class="bannerwrapper" scroll-y="true" @scrolltolower="lower">
+		<!-- <scroll-view class="bannerwrapper" scroll-y="true" @scrolltolower="lower">
 			<image
 				v-for="(item, index) in bannerlist"
 				:key="index"
@@ -8,95 +8,109 @@
 				:style="{ height: `calc(80vh / ${bannerlist.length >= 3 ? 3 : bannerlist.length})` }"
 				:src="item.imageUrl"
 			/>
-		</scroll-view>
+		</scroll-view> -->
+		<up-image :src="bannerlist[0]?.imageUrl" width="100%" height="80vh" @click="handleOpenPage(bannerlist[0].clickUrl)"></up-image>
+		<up-count-down :time="5 * 1000" format="ss" class="count-down" @finish="countDownFinsh"></up-count-down>
 		<view class="logo">
-			<image src="/static/images/logo2.png" mode="">12345</image>
+			<image src="/static/images/logo2.png" mode=""></image>
 		</view>
 	</view>
 </template>
 <script setup>
-import {
-	onMounted,
-	reactive,
-	ref
-} from 'vue';
-import {
-	onLoad,
-	onHide,
-	onReachBottom
-} from '@dcloudio/uni-app'
+import { reactive, ref } from 'vue';
+import { onLoad, onHide, onReachBottom } from '@dcloudio/uni-app';
 
-import {getAdsList} from '@/api/public.js'
+import { getAdsList } from '@/api/public.js';
 
-import {
-	login
-} from '@/api/setup.js'
+import { login } from '@/api/setup.js';
 
-let bannerlist = reactive([])
-const total = ref(0)
-const page = ref(1)
-onMounted(() => {
-	const isFirst = uni.getStorageSync('isFirst')
-	list() //获取轮播图列表
-	if (isFirst === false) {
-		setTimeout(() => {
-			uni.switchTab({
-				// #ifdef APP-PLUS
-				url: '/pages/index/index'
-				// #endif
-				// #ifndef APP-PLUS
-				url: '/pages/index/index-v'
-				// #endif
-			})
-		}, 5000)
-	} else {
-		setTimeout(() => {
-			uni.navigateTo({
-				url: '/pages/guide/guide'
-			})
-		}, 5000)
-	}
+let bannerlist = reactive([]);
 
-})
+const total = ref(0);
+const page = ref(1);
+const isFirst = ref(false)
+
 onLoad(() => {
+	isFirst.value = uni.getStorageSync('isFirst');
+
 	const params = {
 		username: 'demo_user',
 		password: 'password123'
-	}
-	login(params).then(res => {
+	};
+	login(params).then((res) => {
 		if (res.code === 200) {
-			uni.setStorageSync('user_info', res.data.userInfo)
-			uni.setStorageSync('token', res.data.token)
+			uni.setStorageSync('user_info', res.data.userInfo);
+			uni.setStorageSync('token', res.data.token);
+			list(); //获取轮播图列表
 		}
-	})
-})
+	});
+});
 onHide(() => {
-	uni.setStorageSync('isFirst', false)
-})
+	uni.setStorageSync('isFirst', false);
+});
 const list = () => {
 	getAdsList({
 		adType: 'banner',
 		currentPage: page.value,
 		pageSize: 20
-	}).then(res => {
+	}).then((res) => {
 		console.log(res);
-		bannerlist = [...bannerlist, res.data.records]
-		total.value = res.data.total
-	})
-
-}
+		bannerlist = res.data.records;
+		console.log('bannerlist', bannerlist[0].imageUrl);
+		total.value = res.data.total;
+	});
+};
 const lower = () => {
 	if (total.value > bannerlist.length) {
-		total.value++
-		list()
+		total.value++;
+		list();
 	}
+};
 
+// 打开页面
+const handleOpenPage = (url)=>{
+	// #ifdef APP-PLUS
+	plus.runtime.openURL( url, ()=>{
+		console.log('打开外链失败');
+	} );
+	// #endif
+
+	// #ifdef APP-PLUS
+	console.log('手机打开失败');
+	// #endif
+}
+
+// 倒计时结束跳转页面
+const countDownFinsh = ()=>{
+	if (isFirst.value === false) {
+		uni.switchTab({
+			// #ifdef APP-PLUS
+			url: '/pages/index/index'
+			// #endif
+			// #ifndef APP-PLUS
+			url: '/pages/index/index-v'
+			// #endif
+		})
+	} else {
+		uni.navigateTo({
+			url: '/pages/guide/guide'
+		})
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 page {
 	background: #fff;
+}
+
+.count-down {
+	position: fixed;
+	top: 120rpx;
+	right: 80rpx;
+	background-color: rgba(0, 0, 0, 0.3);
+	padding: 5rpx 10rpx;
+	border-radius: 50%;
 }
 
 .bannerwrapper {
