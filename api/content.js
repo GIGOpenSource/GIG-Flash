f797f88e6951a998837c6728a281d78afaf03867
wyz <wyz@gig.com>
2025-08-21 21:50:02 +0800

    发现页对接

--- api/content.js
+++ api/content.js
@@ -1,7 +1,14 @@
-import { getRequest, postRequest } from "@/utils/http";
+import {
+	getRequest,
+	postRequest
+} from "@/utils/http";
 
 // 推荐列表
-export const getContentList =  params => {
-    return getRequest('/content/core/list', params);
+export const getContentList = params => {
+	return getRequest('/content/core/list', params);
 }
 
+// 发现列表
+export const discoverList = params => {
+	return getRequest('/content/core/listForFX', params)
+}
--- pages/discover/TabsPage.vue
+++ pages/discover/TabsPage.vue
@@ -1,57 +1,67 @@
 <template>
-  <z-paging ref="pagingRef" v-model="dataList" @query="queryList">
-    <view class="content">
-      <BannerSwiper />
+	<z-paging ref="pagingRef" v-model="dataList" @query="queryList">
+		<view class="content">
+			<!-- <BannerSwiper /> -->
 
-      <view class="card-list">
-        <card-view v-for="item in 10" @click="handleToLongVideo"></card-view>
-      </view>
-    </view>
-  </z-paging>
+			<view class="card-list">
+				<card-view :item="item" v-for="item in dataList" @click="handleToLongVideo"></card-view>
+			</view>
+		</view>
+	</z-paging>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
 
-defineProps(["tabIndex", "current"]);
+const props = defineProps(['tabIndex', 'current']);
 
-import BannerSwiper from "./components/BannerSwiper/BannerSwiper.vue";
-import CardView from "./components/CardView/CardView.vue";
+import BannerSwiper from './components/BannerSwiper/BannerSwiper.vue';
+import CardView from './components/CardView/CardView.vue';
 
-import { getContentList } from "@/api/content.js";
+import { discoverList } from '@/api/content.js';
 
+const pagingRef = ref();
 const dataList = ref([]);
 
-const queryList = () => {
-  getContentList().then((res) => {
-    console.log("content list res ", res);
-  });
+const queryList = ({ pageNo, pageSize }) => {
+	const params = {
+		contentType: 'LONGVIDEO',
+		selectType: props.current == 0 ? 'F' : 'T',
+		currentPage: pageNo,
+		pageSize
+	};
+	discoverList(params).then((res) => {
+		console.log('content list res ', res);
+		if (res.code === 200) {
+			pagingRef.value.complete(res.data.records);
+		}
+	});
 };
 
 const handleToLongVideo = () => {
-  uni.navigateTo({
-    url: "/pages/video/video",
-  });
+	uni.navigateTo({
+		url: '/pages/video/video'
+	});
 };
 </script>
 
 <style lang="scss" scoped>
 .content {
-  padding: 20rpx;
+	padding: 20rpx;
 }
 
 .card-list {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 20rpx;
+	display: flex;
+	flex-wrap: wrap;
+	margin-top: 20rpx;
 
-  .card-view {
-    margin-right: 30rpx;
-    margin-bottom: 30rpx;
+	.card-view {
+		margin-right: 30rpx;
+		margin-bottom: 30rpx;
 
-    &:nth-child(2n) {
-      margin-right: 0;
-    }
-  }
+		&:nth-child(2n) {
+			margin-right: 0;
+		}
+	}
 }
 </style>
